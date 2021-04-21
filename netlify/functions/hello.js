3f2dae0034cf35bc7a1246c83f9eb2d79918a06f
Nick Taylor <nick@iamdeveloper.com>
2021-04-21 14:18:50 -0400

    Use || for now.

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-  const username = event.path.replace(/\/api\/posts\/?/, '') ?? 'nickytonline'
+  const username = event.path.replace(/\/api\/posts\/?/, '') || 'nickytonline'
   console.dir(username)
   // https://dev.to/api/articles?username=nickytonline
   const response = await fetch(`https://dev.to/api/articles?username=${username}`)
