043e9d9c64895c29111ee6a22acfad91b511800e
Nick Taylor <nick@iamdeveloper.com>
2021-04-21 14:17:19 -0400

    Create posts API

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,14 @@
-exports.handler = async () => {
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const username = event.path.replace(/\/api\/posts\/?/, '') ?? 'nickytonline'
+  console.dir(username)
+  // https://dev.to/api/articles?username=nickytonline
+  const response = await fetch(`https://dev.to/api/articles?username=${username}`)
+  const payload = await response.json();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ greeting: 'YOLO'})
+    body: JSON.stringify(payload)
   }
 }
